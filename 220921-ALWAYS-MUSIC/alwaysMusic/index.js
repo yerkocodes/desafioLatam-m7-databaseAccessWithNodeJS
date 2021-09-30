047c06b184f44dfea59718506bffc2e2ba6f747a
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-09-29 22:59:14 -0300

    Modularized environment configuration

--- 220921-ALWAYS-MUSIC/alwaysMusic/index.js
+++ 220921-ALWAYS-MUSIC/alwaysMusic/index.js
@@ -1,27 +1,12 @@
 require('dotenv').config();
 
 const { Client } = require('pg');
+const config = require('./modules/envConfig');
 
-const addStudent = require('./modules/addStudent');
-const deleteStudent = require('./modules/deleteStudent');
-const getStudentBtRut = require('./modules/getStudentByRut');
-const getStudents = require('./modules/getStudents');
-const updateStudent = require('./modules/updateStudent');
-
-const {
-  USER_DB,
-  HOST,
-  DATABASE,
-  PASSWORD,
-  PORT,
-} = process.env;
-
-const config = {
-  user: USER_DB,
-  host: HOST,
-  database: DATABASE,
-  password: PASSWORD,
-  port: PORT,
-};
+//const addStudent = require('./modules/addStudent');
+//const deleteStudent = require('./modules/deleteStudent');
+//const getStudentBtRut = require('./modules/getStudentByRut');
+//const getStudents = require('./modules/getStudents');
+//const updateStudent = require('./modules/updateStudent');
 
 const client = new Client(config);
