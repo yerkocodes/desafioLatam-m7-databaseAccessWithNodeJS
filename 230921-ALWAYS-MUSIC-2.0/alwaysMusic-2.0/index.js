7c56e2891089c7f184b38aa1f800662926821c03
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-10-03 02:36:46 -0300

    Query functions added in the finished project

--- 230921-ALWAYS-MUSIC-2.0/alwaysMusic-2.0/index.js
+++ 230921-ALWAYS-MUSIC-2.0/alwaysMusic-2.0/index.js
@@ -15,6 +15,10 @@ const pool = new Pool(config);
 pool.connect( async ( err_connect, client, release ) => {
   const option = arguments[0];
   try {
+    if( err_connect ) {
+      throw new Error(error_conexion);
+    };
+    //--------------------------------------------------------
     // Obtener estudiante mediante el rut
     if ( option == 'rut' ) {
       const getStudentByRut = async (rut) => {
@@ -23,90 +27,111 @@ pool.connect( async ( err_connect, client, release ) => {
           text:"select * from estudiantes where rut = $1;",
           values: [rut],
         };
-        const res = await client.query(SQLQuery);
-        release();
-        console.log(res.rows[0]);
+        try {
+          const res = await client.query(SQLQuery);
+          release();
+          console.log(res.rows[0]);
+        } catch ( err ) {
+          console.log(err.message);
+        } finally {
+          client.end();
+        };
       };
       getStudentByRut(arguments[1]);
     };
 
+    // Consultar todos los estudiantes registrados
     if ( option == 'consulta' ) {
-      try {
-        const consultaEstudiantes = async () => {
-          const SQLQuery = {
-            rowMode: 'array',
-            text: "select * from estudiantes",
-          };
+      const consultaEstudiantes = async () => {
+        const SQLQuery = {
+          rowMode: 'array',
+          text: 'select * from estudiantes',
+        };
+        try {
           //const SQLQuery = 'select * from estudiantes;';
           const res = await client.query(SQLQuery);
-          //release();
+          release();
           console.log(res.rows);
+        } catch ( err ) {
+          console.log(err.message);
+        } finally {
+          client.end();
         };
+      };
       consultaEstudiantes();
-      } catch (err) {
-        console.log(err);
-      }
     };
 
+    // Registrar un nuevo estudiante
+    if ( option == 'nuevo' ) {
+      const addStudent = async (nombre, rut, curso, nivel) => {
+        const SQLQuery = {
+          name: 'fetch-user',
+          text: 'insert into estudiantes values ($1,$2,$3,$4) RETURNING *',
+          values: [nombre, rut, curso, nivel],
+        };
+        try {
+          const res = await client.query(SQLQuery);
+          release();
+          console.log(`Estudiante ${nombre} agregado con exito!`);
+          console.log(res.rows[0]);
+        } catch(err) {
+          console.log(err.message);
+        } finally {
+          client.end();
+        };
+
+      };
+      addStudent(arguments[1],arguments[2],arguments[3],arguments[4]);
+    };
+
+    // Editar un estudiante existente mediante su nombre
+    if ( option == 'editar' ) {
+      const updateStudent = async ( rut, curso, nivel, nombre ) => {
+        const SQLQuery = {
+          name: 'fetch-user',
+          text: 'update estudiantes set rut = $1, curso = $2, nivel = $3 where nombre = $4 RETURNING *',
+          values: [ rut, curso, nivel, nombre ],
+        };
+        try {
+          const res = await client.query(SQLQuery);
+          release();
+          console.log(`Estudiante ${nombre} editado con exito.`);
+          console.log(res.rows[0]);
+        } catch (err) {
+          console.log(err.message);
+        } finally {
+          client.end();
+        };
+      };
+      updateStudent(arguments[1],arguments[2],arguments[3],arguments[4])
+    };
+
+    // Eliminar un estudiante mediante su rut
+    if ( option == 'eliminar' ) {
+      const deleteStudent = async (rut) => {
+        const SQLQuery = {
+          name: 'fetch-user',
+          text: 'delete from estudiantes where rut = $1;',
+          values: [ rut ],
+        };
+        try {
+          const res = await client.query(SQLQuery);
+          release();
+          console.log(`Registro de estudiante con rut ${rut} eliminado.`);
+        } catch(err) {
+          console.log(err.message);
+        } finally {
+          client.end();
+        };
+      };
+      deleteStudent(arguments[1]);
+
+    };
+    //--------------------------------------------------------
   } catch ( err ) {
     console.log(err.message);
   } finally {
-    //client.end();
-    pool.end();
+    pool.end() ;
   };
 
 });
-
-
-//switch ( option ) {
-
-//case 'consulta':
-//break;
-
-//case 'nuevo':
-//const addStudent = async (nombre, rut, curso, nivel) => {
-//try {
-//const res = await client.query(`insert into estudiantes values ('${nombre}','${rut}','${curso}',${Number(nivel)})`);
-//console.log(`Estudiante ${nombre} agregado con exito!`);
-//} catch(err) {
-//console.log(err.message);
-//} finally {
-//client.end();
-//};
-//};
-//addStudent(arguments[1],arguments[2],arguments[3],arguments[4]);
-//break;
-
-//case 'editar':
-//const updateStudent = async (nombre, rut, curso, nivel) => {
-//try {
-//const res = await client.query(`update estudiantes set nombre = '${nombre}', curso = '${curso}', nivel = ${Number(nivel)} where rut = '${rut}'`);
-//console.log(`Estudiante ${nombre} editado con exito.`);
-//} catch (err) {
-//console.log(err.message);
-//} finally {
-//client.end();
-//};
-//};
-//updateStudent(arguments[1],arguments[2],arguments[3],arguments[4])
-//break;
-
-//case 'eliminar':
-//const deleteStudent = async (rut) => {
-//try {
-//const res = await client.query(`delete from estudiantes where rut = '${rut}';`)
-//console.log(`Registro de estudiante con rut ${rut} eliminado.`);
-//} catch(err) {
-//console.log(err.message);
-//} finally {
-//client.end();
-//};
-//};
-//deleteStudent(arguments[1]);
-//break;
-
-//default:
-//console.log('Debe ingresar una instruccion valida.');
-//client.end();
-//break;
-//};
